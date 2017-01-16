8a4a53a449936db8f0680cea866bc640d67ddd4e
Fabian Martin <fabianmartin97@gmail.com>
2017-01-16 23:39:14 +0100

    Removed unnecessary comments

--- Presenter/PresenterLoader.java
+++ Presenter/PresenterLoader.java
@@ -7,16 +7,8 @@ public class PresenterLoader<T extends MvpPresenter> extends Loader<T> {
     private final PresenterFactory<T> factory;
     private T presenter;
 
-    /**
-     * Stores away the application context associated with context.
-     * Since Loaders can be used across multiple activities it's dangerous to
-     * store the context directly; always use {@link #getContext()} to retrieve
-     * the Loader's Context, don't use the constructor argument directly.
-     * The Context returned by {@link #getContext} is safe to use across
-     * Activity instances.
-     *
-     * @param context used to retrieve the application context.
-     */
+    // Stores away the application context associated with context.
+     
     public PresenterLoader(Context context, PresenterFactory<T> factory) {
         super(context);
         this.factory = factory;
@@ -24,20 +16,18 @@ public class PresenterLoader<T extends MvpPresenter> extends Loader<T> {
 
     @Override
     public void onStartLoading() {
-        // System.out.println("onStartLoading. Presenter = " + (presenter == null ? null : "Pres."));
         // If we already own an instance, simply deliver it.
         if (presenter != null) {
             deliverResult(presenter);
             return;
         }
 
-        // Otherwise, force a load
+        // Otherwise, create a new one
         forceLoad();
     }
 
     @Override
     public void onForceLoad() {
-        // System.out.println("onForceLoad. Presenter = " + (presenter == null ? null : "Pres."));
         // Create the Presenter using the Factory
         presenter = factory.create();
 
@@ -47,7 +37,6 @@ public class PresenterLoader<T extends MvpPresenter> extends Loader<T> {
 
     @Override
     public void onReset() {
-        // System.out.println("onReset. Presenter = " + (presenter == null ? null : "Pres."));
         if (presenter != null) {
             presenter.onDestroyed();
             presenter = null;
