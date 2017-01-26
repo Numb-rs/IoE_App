76d701e5eaaf4b5d087214c4baabd3f8ebbfcf9c
Fabian Martin <fabianmartin97@gmail.com>
2017-01-26 01:29:53 +0100

    Added comments and some minor changes

--- View/MvpView.java
+++ View/MvpView.java
@@ -1,9 +1,30 @@
 package internetofeveryone.ioe.View;
 
+/**
+ * Created by Fabian Martin for 'Internet of Everyone'
+ *
+ * This interface represents the view for the whole application
+ */
 public interface MvpView {
 
+    /**
+     * Handles the start of a view
+     */
     public void onStart();
+
+    /**
+     * Handles the stopping of an view
+     */
     public void onStop();
+
+    /**
+     * Handles the pausing of an view
+     */
     public void onPause();
+
+    /**
+     * Updates the data
+     */
+    public void dataChanged();
 
 }
